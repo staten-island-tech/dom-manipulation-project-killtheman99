07a48d73b04ec263b2e214a930bb7a1fa42e1ef6
Antongin <antong19@nycstudents.net>
2022-11-10 14:01:38 -0500

    stuff

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   box: document.getElementById("black-box"),
+  points: document.querySelectorAll(".points"),
 };
+
 function backgroundandtext(background, text) {
   background.style.backgroundColor = "blue";
-  text.textcontent = "This is now a blue box";
+  text.textContent = "This is now a blue box";
   text.style.fontSize = "30px";
 }
 
 DOMSelectors.button.addEventListener("click", function () {
   backgroundandtext(DOMSelectors.box, DOMSelectors.text);
 });
+
+function changeLi() {
+  DOMSelectors.points.forEach((point) => {})
+  point.addEventListener("click", function)
+}
+changeLi();
