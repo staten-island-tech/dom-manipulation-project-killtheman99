811e1909cded97459b44ce46e3b7017b66bed993
Antongin <antong19@nycstudents.net>
2022-11-14 13:58:04 -0500

    stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const DOMSelectors = {
   text: document.querySelector("#text"),
   box: document.getElementById("black-box"),
   points: document.querySelectorAll(".points"),
+  input: document.querySelector("#input"),
 };
 
 function backgroundandtext(background, text) {
@@ -16,7 +17,12 @@ DOMSelectors.button.addEventListener("click", function () {
 });
 
 function changeLi() {
-  DOMSelectors.points.forEach((point) => {})
-  point.addEventListener("click", function)
+  let pointindex = 1;
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.textContent = `hello I am point ${pointindex}`;
+      pointindex++;
+    });
+  });
 }
 changeLi();
