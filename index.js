8f055a3e5dc4d416ae54002f7a119aa1082eb011
Antongin <antong19@nycstudents.net>
2022-11-16 13:58:51 -0500

    stuff

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const DOMSelectors = {
   text: document.querySelector("#text"),
   box: document.getElementById("black-box"),
   points: document.querySelectorAll(".points"),
-  input: document.querySelector("#input"),
+  input: document.querySelector(`#input`),
   button2: document.getElementById("btn2"),
+  input2: document.querySelector(`#input2`),
 };
 
 function backgroundandtext(background, text) {
@@ -17,6 +18,18 @@ DOMSelectors.button2.addEventListener("click", function () {
   backgroundandtext(DOMSelectors.box, DOMSelectors.text);
 });
 
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  let input2 = DOMSelectors.input2.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<p>${input}</p>`,
+    `<p>${input2}</p>`
+  );
+  DOMSelectors.input.value = "";
+  DOMSelectors.input2.value = "";
+});
+
 function changeLi() {
   let pointindex = 1;
   DOMSelectors.points.forEach((point) => {
@@ -27,8 +40,3 @@ function changeLi() {
   });
 }
 changeLi();
-
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors(input, input2).value;
-  console.log(input);
-});
