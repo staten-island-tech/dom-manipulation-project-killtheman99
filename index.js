395a310cb4802659f1409d67c44f0b71de8a22bb
Antongin <antong19@nycstudents.net>
2022-11-17 13:47:18 -0500

    done for now

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const DOMSelectors = {
   input: document.querySelector(`#input`),
   button2: document.getElementById("btn2"),
   input2: document.querySelector(`#input2`),
+  button3: document.getElementById("btn3"),
 };
 
 function backgroundandtext(background, text) {
@@ -21,9 +22,17 @@ DOMSelectors.button2.addEventListener("click", function () {
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   let input2 = DOMSelectors.input2.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input} ${input2}</p>`);
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<p class="name">${input} ${input2}</p>`
+  );
   DOMSelectors.input.value = "";
   DOMSelectors.input2.value = "";
+});
+
+DOMSelectors.button3.addEventListener("click", function () {
+  let nametags = document.querySelectorAll(".name");
+  nametags.forEach((name) => name.remove());
 });
 
 function changeLi() {
