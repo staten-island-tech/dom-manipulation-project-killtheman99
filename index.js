658c7d0dc8af48e1f8f1927e31eb71d74b7d02f0
Antongin <antong19@nycstudents.net>
2022-11-29 14:01:54 -0500

    fsdfs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const DOMSelectors = {
   button2: document.getElementById("btn2"),
   input2: document.querySelector(`#input2`),
   button3: document.getElementById("btn3"),
+  button4: document.querySelectorAll(".remove"),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
@@ -21,22 +22,25 @@ DOMSelectors.button.addEventListener("click", function () {
   );
   DOMSelectors.input.value = "";
   DOMSelectors.input2.value = "";
-  let remove = document.getElementById("btn4");
-  console.log(remove);
-  remove.addEventListener("click", function (remove) {
-    remove.parentElement.remove();
-  });
-});
-
-function card() {
+  let remove = document.querySelectorAll(".remove");
   remove.forEach((remove) => {
-    remove.addEventListener("click", function (obj) {
-      obj.parentElement.remove();
+    remove.addEventListener("click", function () {
+      let thingy = document.querySelectorAll(".thingy");
+      remove.remove();
+      thingy.remove();
     });
   });
-}
 
-DOMSelectors.button3.addEventListener("click", function () {
-  let nametags = document.querySelectorAll(".name");
-  nametags.forEach((name) => name.remove());
+  //function card() {
+  //remove.forEach((remove) => {
+  // remove.addEventListener("click", function (obj) {
+  // obj.parentElement.remove();
+  // });
+  // });
+  //}
+
+  DOMSelectors.button3.addEventListener("click", function () {
+    let nametags = document.querySelectorAll(".name");
+    nametags.forEach((name) => name.remove());
+  });
 });
